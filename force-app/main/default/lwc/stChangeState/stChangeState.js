0c4641c7f51dfa31fc95cb63f8d7ed1beafae20e
Dilip Mistry <Dilip.Kumar.Mistry@gmail.com>
2023-03-09 11:16:46 +0000

    new commit

--- force-app/main/default/lwc/stChangeState/stChangeState.js
+++ force-app/main/default/lwc/stChangeState/stChangeState.js
@@ -1,4 +1,5 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class StChangeState extends LightningElement {
     @api cardTitle;
@@ -6,4 +7,19 @@ export default class StChangeState extends LightningElement {
     @api fieldNames;
     @api changeBtnLabel;
     @api recordId;
+
+    @track fields = [];
+
+    connectedCallback() {
+        this.fields = this.fieldNames.split(",").map(item => item.trim());
+    }
+    handleSuccess() {
+        const event = new ShowToastEvent({
+            title: 'Success',
+            variant: 'success',
+            message:
+                'Record has been updated successfully!',
+        });
+        this.dispatchEvent(event);
+    }
 }
