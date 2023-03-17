4626da183f38f6d27548bdcf2f24113d37207eae
Dilip Mistry <Dilip.Kumar.Mistry@gmail.com>
2023-03-17 17:12:57 +0000

    added multi-select dropdown

--- force-app/main/default/lwc/stChangeState/stChangeState.js
+++ force-app/main/default/lwc/stChangeState/stChangeState.js
@@ -1,18 +1,46 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
 export default class StChangeState extends LightningElement {
     @api cardTitle;
-    @api objectName;
     @api fieldNames;
     @api changeBtnLabel;
+
+    @api objectApiName;
     @api recordId;
 
-    @track fields = [];
+    @track record;
+    @track objectInfo;
+    @track fields = []; // to display on the page/html
+    @track fieldList = []; // to get the details of the record
 
     connectedCallback() {
-        this.fields = this.fieldNames.split(",").map(item => item.trim());
+        this.fields = this.fieldNames.split(",").map((item) => {
+            return { fieldName: item.trim() };
+        });
+
+        this.fieldList = this.fieldNames.split(",").map(item => item.trim());
+        this.fieldList = [...this.fieldList, "RecordTypeId"];
+        console.log(this.fieldList, "Field List");
     }
+
+    renderedCallback() {
+    }
+
+    @wire(getRecord, { recordId: '$recordId', fields: '$fieldList' })
+    wiredRecordss({ error, data }) {
+        if (data) {
+            this.record = data;
+            console.log(data, "data");
+            console.log(data.Status, "data.Status");
+            console.log(getFieldValue(data, 'Case.Status'));
+        } else {
+            console.log(error, "error");
+        }
+    }
+
     handleSuccess() {
         const event = new ShowToastEvent({
             title: 'Success',
--- force-app/main/default/lwc/stTransitionSettings/stTransitionSettings.js
+++ force-app/main/default/lwc/stTransitionSettings/stTransitionSettings.js
@@ -1,22 +1,24 @@
-import { LightningElement, track } from 'lwc';
-import getAllObjects from '@salesforce/apex/ST_TransitionSettingsController.getObjects';
+import { LightningElement, track, wire,api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getObjects from '@salesforce/apex/ST_TransitionSettingsController.getObjects';
 import getFields from '@salesforce/apex/ST_TransitionSettingsController.getObjectFields';
+import getPicklistValues from '@salesforce/apex/ST_TransitionSettingsController.getPicklistValues';
+import getProfilesName from '@salesforce/apex/ST_TransitionSettingsController.getProfilesName';
+import dmlOnStateTransition from '@salesforce/apex/ST_TransitionSettingsController.dmlOnStateTransition';
 
 export default class StTransitionSettings extends LightningElement {
     @track listObjects;
     @track listObjectFields;
+    @track listFieldValues;
+    @track listProfilesName;  
+    @track isLoading = true;
+    selectedOptions = [];
     @track error;
 
-    @track selectedObject;
-    @track selectedField;
+    @api selectedObject;
+    @api selectedField;
 
     @track transitionData = [];
-    @track columns = [
-        { label: 'From', fieldName: 'from' },
-        { label: 'To', fieldName: 'to' },
-        { label: 'Allowed Profiles', fieldName: 'allowedProfiles' }
-    ];
-
     validTransition = [
         { from: "New", to: "In Progress" },
         { from: "In Progress", to: "Closed" },
@@ -25,51 +27,156 @@ export default class StTransitionSettings extends LightningElement {
         { from: "Reopen", to: "In Progress" }
     ];
 
+    get showTransitionSection(){
+        return this.selectedObject && this.selectedField;
+    }
 
-    connectedCallback() {
-        getAllObjects()
-            .then(result => {
-                if (result) {
-                    this.listObjects = [{ value: '', label: '' }];
-                    for (var key in result) {
-                        this.listObjects.push({ label: result[key], value: key }); //Here we are creating the array to show on UI.
-                    }
-                    this.listObjects = this.sortDataByValue(this.listObjects);
-                }
+    @wire(getObjects)
+    wiredObjectsName({ error, data }) {
+        if (data) {            
+            console.log('@@@ wiredObjectsName');
+            this.listObjects = [];//[{ value: '', label: '' }];
+            for (var key in data) {
+                this.listObjects.push({ label: data[key], value: key }); //Here we are creating the array to show on UI.
+            }
+            this.listObjects = this.sortDataByValue(this.listObjects);
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+        }
+    }
 
-            })
-            .catch(error => {
-                this.error = error;
-            });
-
+    sortDataByValue(records) {
+        return records.sort((a, b) => (a.label < b.label) ? -1 : ((b.label > a.label) ? 1 : 0))
     }
 
     handleObjectChange(event) {
         this.selectedObject = event.detail.value;
         this.selectedField = null;
+        console.log('this.selectedObject - ',this.selectedObject);
 
         getFields({ objectName: this.selectedObject })
-            .then((result) => {
-                if (result) {
-                    this.listObjectFields = [{ value: "", label: "" }];
-                    for (var key in result) {
-                        this.listObjectFields.push({ label: result[key], value: key }); //Here we are creating the array to show on UI.
-                    }
-                    this.listObjectFields = this.sortDataByValue(this.listObjectFields);
+        .then((result) => {
+            if (result) {
+                this.listObjectFields = [];//[{ value: "", label: "" }];
+                for (var key in result) {
+                    this.listObjectFields.push({ label: result[key], value: key }); //Here we are creating the array to show on UI.
                 }
+                this.listObjectFields = this.sortDataByValue(this.listObjectFields);
+            }
+        })
+        .catch((error) => {
+            this.error = error;
+        });
+    }    
 
-            })
-            .catch((error) => {
-                this.error = error;
-            });
-    }
+    //show/hide spinner
+    handleIsLoading(isLoading) {
+        this.isLoading = isLoading;
+    }  
 
     handleFieldChange(event) {
         this.selectedField = event.detail.value;
-        console.log("Selected Field:" + this.selectedField);
+        console.log('this.selectedField - ',this.selectedField);
+
+        getPicklistValues({ objectName: this.selectedObject, fieldName: this.selectedField })
+        .then((result) => {
+            if (result) {
+                this.listFieldValues = [];//[{ value: "", label: "" }];
+                for (var key in result) {
+                    this.listFieldValues.push({ label: result[key], value: key }); //Here we are creating the array to show on UI.
+                }
+                this.listFieldValues = this.sortDataByValue(this.listFieldValues);
+            }
+        })
+        .catch((error) => {
+            this.error = error;
+        });
     }
 
-    sortDataByValue(records) {
-        return records.sort((a, b) => (a.label < b.label) ? -1 : ((b.label > a.label) ? 1 : 0))
+    @wire(getProfilesName)
+    wiredProfileName({ error, data }) {
+        if (data) {
+            console.log('@@@ wiredObjectsName ---');
+            this.listProfilesName = [];
+            for (var key in data) {
+                this.listProfilesName.push({ label: data[key], value: key });
+            }
+            this.listProfilesName = this.sortDataByValue(this.listProfilesName);
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.listProfilesName = undefined;
+        }
+    } 
+
+    handleChange(event) {
+        this.selectedOptions = event.detail;
+    }
+
+    keyIndex = 0;
+    @track records = [
+        {   
+            Object__c: this.selectedObject,
+            Field__c: this.selectedField
+        }
+    ];
+
+    //to add row
+    addRow() {
+        console.log('addRow');
+        ++this.keyIndex;
+        let myNewElement = {From_State__c: "", To_State__c: "", Id: this.keyIndex, Allowed_Profiles__c: ""};
+        this.records = [...this.records, myNewElement];
+    }
+
+    //update table row values in list
+    updateValues(event) {
+        var foundelement = this.records.find(ele => ele.Id == event.target.dataset.id);
+        if (event.target.name === 'From') {
+            foundelement.From_State__c = event.target.value;
+        } else if (event.target.name === 'To') {
+            foundelement.To_State__c = event.target.value;
+        } else if (event.target.name === 'AllowedProfiles') {
+            foundelement.Allowed_Profiles__c = event.detail.map((item) => item.label).join(",");
+        }
+    }
+
+    //handle save and process dml 
+    handleSaveAction(){
+        this.records.forEach(res => {            
+            if (!isNaN(res.Id)) {
+                res.Id = null;
+            } 
+            if(res.Object__c==undefined){
+                res.Object__c= this.selectedObject;
+            } 
+            if(res.Field__c==undefined){
+                res.Field__c= this.selectedField;
+            }
+        }); 
+        //var parsedRecords = JSON.parse(JSON.stringify(this.records));
+        dmlOnStateTransition({data: this.records})
+        .then( result => {
+            this.showToast('Success', result, 'Success', 'dismissable');
+        }).catch( error => {
+            console.log(error);
+            this.showToast('Error updating or refreshing records', error.body.message, 'Error', 'dismissable');
+        });
+    }
+
+    removeRow(event) {
+        console.log('removeRow -- '+JSON.stringify(this.records));
+        this.records.splice(this.records.findIndex(row => row.Id === event.target.dataset.id), 1);
+    }    
+
+    showToast(title, message, variant, mode) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+            mode: mode
+        });
+        this.dispatchEvent(event);
     }
 }
