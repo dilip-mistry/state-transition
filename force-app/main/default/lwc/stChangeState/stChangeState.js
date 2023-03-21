5aa9e78f890ceec1e5a151807e3b6519211aa89f
Dilip Mistry <Dilip.Kumar.Mistry@gmail.com>
2023-03-21 09:03:17 +0000

    fully Working StateChange component

--- force-app/main/default/lwc/stChangeState/stChangeState.js
+++ force-app/main/default/lwc/stChangeState/stChangeState.js
@@ -1,43 +1,134 @@
 import { LightningElement, api, track, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import getAllowedTransitions from '@salesforce/apex/ST_TransitionFlowUtility.getAllowedTransitionsFrom';
+import { updateRecord } from "lightning/uiRecordApi";
 
 export default class StChangeState extends LightningElement {
     @api cardTitle;
-    @api fieldNames;
+    @api fieldApiName;
     @api changeBtnLabel;
 
-    @api objectApiName;
     @api recordId;
+    @api objectApiName;
 
-    @track record;
-    @track objectInfo;
-    @track fields = []; // to display on the page/html
-    @track fieldList = []; // to get the details of the record
+    @track selectedState;
+    @track currentState;
+    @track allowedToStates;
 
-    connectedCallback() {
-        this.fields = this.fieldNames.split(",").map((item) => {
-            return { fieldName: item.trim() };
-        });
+    recordTypeId = "012000000000000AAA";
+    fetchRecTypeField = false;
+    currentRecord;
 
-        this.fieldList = this.fieldNames.split(",").map(item => item.trim());
-        this.fieldList = [...this.fieldList, "RecordTypeId"];
-        console.log(this.fieldList, "Field List");
+    picklistGetterObject;
+    allPicklistOptions;
+
+    objectInfo;
+    fieldInfo;
+
+    get fieldLabel() {
+        return this.fieldInfo?.label;
     }
 
-    renderedCallback() {
+
+    get fieldsToFetch() {
+        var fields = [];
+        fields.push(this.objectApiName + "." + this.fieldApiName);
+        if (this.fetchRecTypeField == true) {
+            fields.push(this.objectApiName + ".RecordTypeId");
+        }
+        return fields;
     }
 
-    @wire(getRecord, { recordId: '$recordId', fields: '$fieldList' })
-    wiredRecordss({ error, data }) {
+    @wire(getRecord, { recordId: '$recordId', fields: '$fieldsToFetch' })
+    wiredCurrentRecord({ error, data }) {
         if (data) {
-            this.record = data;
-            console.log(data, "data");
-            console.log(data.Status, "data.Status");
-            console.log(getFieldValue(data, 'Case.Status'));
-        } else {
-            console.log(error, "error");
+            console.log(data);
+            this.currentState = data.fields[this.fieldApiName]?.value;
+            if (this.fetchRecTypeField == true) {
+                this.recordTypeId = data.fields["RecordTypeId"]?.value;
+            }
+        }
+    }
+
+    @wire(getObjectInfo, { objectApiName: "$objectApiName" })
+    wiredObjectInfo({ error, data }) {
+        if (data) {
+            this.objectInfo = data;
+
+            if (this.objectInfo.fields[this.fieldApiName] !== undefined) {
+                this.fieldInfo = this.objectInfo.fields[this.fieldApiName];
+
+                console.log(JSON.stringify(this.fieldInfo));
+            }
+
+            if (this.objectInfo.fields["RecordTypeId"] !== undefined) {
+                this.fetchRecTypeField = true;
+            }
+            this.getFieldDetails();
+        }
+        else {
+            console.log("getObjectInfo error", error);
+        }
+    }
+
+    @wire(getPicklistValues, { recordTypeId: '$recordTypeId', fieldApiName: '$picklistGetterObject' })
+    wiredGetPicklistValues({ error, data }) {
+        if (data) {
+            this.allPicklistOptions = data;
+            //this.prepareAllowedTransitions();
+        }
+        else {
+            console.log("getPicklistValues error", error);
+        }
+    }
+
+    @wire(getAllowedTransitions, { objName: '$objectApiName', fieldName: '$fieldApiName', fromState: '$currentState' })
+    wiredAllowedTransitions({ error, data }) {
+        if (data) {
+            this.allowedToStates = (this.allPicklistOptions?.values).filter(state => data.includes(state.value));
+        }
+        else {
+            console.log("getAllowedTransitions error", error);
+        }
+    }
+
+    getFieldDetails() {
+        switch (this.fieldInfo?.dataType) {
+            case 'Picklist':
+                this.picklistGetterObject = {
+                    objectApiName: this.objectApiName,
+                    fieldApiName: this.fieldApiName
+                };
+                break;
+        }
+    }
+
+    handleStateChange(event) {
+        this.selectedState = event.detail.value;
+    }
+
+    updateRecord(event) {
+        console.log("update clicked with new value:" + this.selectedState);
+
+        if (this.selectedState) {
+            const fields = {};
+            fields["Id"] = this.recordId;
+            fields[this.fieldApiName] = this.selectedState;
+
+            updateRecord({ fields: fields })
+                .then((record) => {
+                    console.log(record);
+                    this.currentState = this.selectedState;
+                    this.selectedState = null;
+
+                    this.handleSuccess();
+                })
+                .catch((error) => {
+                    console.log("updateRecord error", error);
+                });
         }
     }
 
